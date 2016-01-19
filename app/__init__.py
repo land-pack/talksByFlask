6408505c4f7a95aedb239c509f79a6bf978349bb
landpack <landpack@sina.com>
2016-01-19 17:46:13 +0800

    version 0.3

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import config
+# pip install flask-bootstrap
+from flask.ext.bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-
+    bootstrap.init_app(app)
     from .talks import talks as talks_blueprint
     app.register_blueprint(talks_blueprint)
 
